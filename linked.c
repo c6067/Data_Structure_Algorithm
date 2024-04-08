be3c75b492f1115557395b4e97b51e2e9e778732
c6067 <121327109+c6067@users.noreply.github.com>
2024-04-08 14:50:50 +0800

    Update linked.c

--- linked.c
+++ linked.c
@@ -188,4 +188,153 @@ void linked_destroy(linked **node){
 	*node = NULL;
 }
 
+linked *linked_reverse(linked *node){	
+	if(node==NULL){
+		return NULL;
+	}
+
+	/* linked* prev = NULL;
+	linked* back = NULL;
+	linked* current = node;
+	while(current != NULL){
+		back = current->next;
+		current->next = prev;
+		prev = current;					//reverse means null pointer move previous
+		current = back;
+	}	
+	return prev; */
+
+	//push front method
+	linked *current = node;
+	linked *head = NULL;
+	while(current){
+		linked *temp = current->next;
+		current->next = head;			//reverse,head local is current back
+		head = current;
+		current = temp;
+	}
+	
+	return head;
+}
+
+//search middle value(single linked list)
+linked *find_middle(linked *node) {
+    //fast&slow pointer method	
+    linked *fast = node;    
+    linked *slow = node;
+    while (fast && fast->next) {        
+        fast = fast->next->next;		//fast pointer move two step,slow move one
+        slow = slow->next;			//such as slow equal middle value. amazimg!!
+    }
+    
+    return slow;
+}
+
+//linked list merge
+linked *merge_two(linked *list1, linked *list2){
+    linked *head = NULL;
+    linked *tail = NULL;
+
+    //key:compare two list's value,littel please to list1;big please list2
+    //traverse continue
+    while (list1 && list2) {    	   
+        if (list1->data <= list2->data) {        	  	
+            head = tail = list1;
+            list1 = list1->next;
+        } else {        	  	
+            head = tail = list2;
+            list2 = list2->next;
+        }        
+        tail = tail->next;
+    }
+    //deal with list2 node of left	
+    if(list2){
+        tail->next = list2; 		    	
+    }
+    return head;
+}
+
+
+//partitioning linked list
+linked *partition(linked *head, int key) {
+    //define two new null linked list    
+    linked *small_head = NULL;
+    linked *small_tail = NULL;
+    linked *great_head = NULL;
+    linked *great_tail = NULL;    
+    //traverse original linked list
+    while (head) {        
+        if (head->data < key) {        	  
+            if (small_head == NULL) {
+                small_head = small_tail = head;
+            } else {
+            	 //push back
+                small_tail->next = head;
+                small_tail = head;
+            }
+        } else {
+        	  //if current data greater than key
+            if (great_head == NULL) {
+                great_head = great_tail = head;
+            } else {            	 
+                great_tail->next = head;
+                great_tail = head;
+            }
+        }        
+        head->next = NULL;        
+        head = head->next;
+    }
+    
+    if (small_head == NULL) {
+        return great_head;
+    } else {    	   	
+        small_tail->next = great_head;
+        return small_head;
+    }
+}
+
+//hashlist***
+linked *intersection_hashlist(linked *head_a, linked *head_b) {    
+   linked* node_set[10000] = { NULL };    
+	
+    /*(intptr_t) is type conversion operator.
+    / it's to use converts the pointer address to an integer of type intptr_t, 
+    / this is to use pointer address as an index of hash table, in order to 
+    / store and find the address of node */
+    while (head_a) {    	
+        node_set[(intptr_t)head_a] = head_a;
+        head_a = head_a->next;
+    }    
+    while (head_b) {
+    	//compare with head_a node
+        if (node_set[(intptr_t)head_b] == head_b) {
+            return head_b;
+        }
+        head_b = head_b->next;
+    }       
+    return NULL;	 //not found result
+}
+
+//double pointer method
+linked *intersection_dp(linked *head1, linked *head2) {    
+    linked *p1 = head1;
+    linked *p2 = head2;
+    
+    //double pointer alternate traverse linked lists,until found or traverse to end
+    while (p1 != p2) {
+        if (p1 == NULL) {
+            p1 = head2;			//p1 list' tail link to list2' head
+        } else {
+            p1 = p1->next;
+        }        
+        
+        if (p2 == NULL) {
+            p2 = head1;
+        } else {
+            p2 = p2->next;
+        }
+    }    
+    
+    return p1;
+}
 
